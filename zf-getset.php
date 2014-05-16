d7f2ca989e20753782bee1aceb54e5cc38721df7
Lando <orlandosanchezjr@gmail.com>
2014-05-15 20:39:16 -0400

    Create zf-getset.php

---
+++ zf-getset.php
@@ -0,0 +1,31 @@
+<?php
+$scriptName   = array_shift($argv);
+$className    = array_shift($argv);
+echo '<?php';
+?>
+
+class <?=$className;?> {
+<?php foreach($argv as $prop): ?>
+  public $<?php echo $prop;?>;
+<?php endforeach; ?>
+
+  public function exchangeArray($data) {
+<?php foreach($argv as $prop): ?>
+    $this->$<?php echo $prop;?> = ($data['<?php echo $prop;?>']) ? $data['<?php echo $prop;?>'] : NULL;
+<?php endforeach; ?>
+  }
+       
+<?php foreach($argv as $prop): 
+    $methodName= implode('', array_map(function($token) {
+      return ($token == 'id') ? strtoupper($token) : ucfirst($token);  
+    }, explode('_', $prop)));?>
+  public function get<?php echo $methodName;?>() {
+    return $this-><?php echo $prop;?>;
+  }
+  public function set<?php echo $methodName;?>(<?php echo $prop;?>) {
+    $this-><?php echo $prop;?> = <?echo $prop;?>;
+  }
+
+<?php endforeach; ?>
+}
+<?php echo '?>';
